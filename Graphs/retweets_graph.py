018d7665ae60ee97dba377cd7386a2b290691689
imoutidi <imoutidi@gmail.com>
2023-06-02 12:37:01 +0200

    Working on network backboning

--- Graphs/retweets_graph.py
+++ Graphs/retweets_graph.py
@@ -84,7 +84,7 @@ class RTGraph:
                 counter += 1
                 edge_file.write(str(rt_pair[0]) + "," + str(rt_pair[1]) + "," + str(num_of_rts) + "\n")
 
-        # Creating the node file first
+        # Creating the node file
         with open(self.output_path + r"Graph_files\1_percent\1p_nodes.csv", "w") as node_file:
             node_file.write("id,label\n")
             for node in percent_nodes:
@@ -93,18 +93,66 @@ class RTGraph:
                 else:
                     node_file.write(str(node) + "," + str(node) + "\n")
 
+    # Keeping retweets only between the top X percent of users based on the number of retweets they have received
+    # in-degree in the retweet di_graph
+    def connections_of_authorities(self):
+        set_of_all_nodes = set()
+        list_of_pairs = tools.load_pickle(self.output_path + r"bin\list_of_rt_edges_with_duplicates")
+        for rt_pair in list_of_pairs:
+            set_of_all_nodes.add(rt_pair[0])
+            set_of_all_nodes.add(rt_pair[1])
+        counted_list_of_pairs = Counter(list_of_pairs)
+        for rt_pair, num_of_rts in counted_list_of_pairs.items():
+            self.directed_graph.add_edge(rt_pair[0], rt_pair[1], weight=num_of_rts)
+        in_degrees = list(self.directed_graph.in_degree)
+        # This is 5% of all the nodes on the retweet network.
+        top_in_degrees = sorted(in_degrees, key=itemgetter(1), reverse=True)#[:180000]
+        # indexing for lookups
+        # top_users = {s[0] for s in top_in_degrees}
+        list_of_top_pairs = dict()
+        for idx, top_user in enumerate(top_in_degrees):
+            print(idx)
+            for rt_pair, num_of_rts in counted_list_of_pairs.items():
+                # this checks if both users of the edge ar in the top X percent of the in-degree users.
+                # if top_user[0] == rt_pair[0] and rt_pair[1] in top_users:
+                list_of_top_pairs[rt_pair] = num_of_rts
+        # removing self retweets.
+        pairs_to_be_removed = list()
+        for top_pair in list_of_top_pairs:
+            if top_pair[0] == top_pair[1]:
+                pairs_to_be_removed.append(top_pair)
+        for t_pair in pairs_to_be_removed:
+            del list_of_top_pairs[t_pair]
+        # tools.save_pickle(self.output_path + r"bin\list_of_top_pairs_10_percent_no_self_rt", list_of_top_pairs)
+        tools.save_pickle(self.output_path + r"bin\list_of_all_pairs_no_self_rt", list_of_top_pairs)
 
-    # This will be deprecated
+
+    def creation_of_digraph(self):
+        list_of_top_pairs = tools.load_pickle(self.output_path + r"bin\list_of_top_pairs_1_percent_no_self_rt")
+        for rt_pair, num_of_rts in list_of_top_pairs.items():
+            self.directed_graph.add_edge(rt_pair[0], rt_pair[1], weight=num_of_rts)
+
+    def mongo_tests(self):
+        dumy_list = [1, 2, 3, 4, 5, 6]
+        db = self.client.Climate_Change_Tweets
+        collection = db.test_base
+        collection.insert_one({"author_id": 1, "author_username": "gg", "id_list": dumy_list})
+
+
+    def create_graph_file_for_backboning(self):
+        print()
+
+
+    def backboning_graph(self):
+        table, nnodes, nnedges = backboning.read("/path/to/input", "weight")
+        nc_table = backboning.noise_corrected(table)
+        nc_backbone = backboning.thresholding(nc_table, threshold_value)
+        backboning.write(nc_backbone, "network_name", "nc", "/path/to/output")
+        print()
+
+    # This will be deprecated (it replaced author ids with selected username which contain a lot of
+    # non utf-8 characters and duplicate names.)
     def create_csv_file(self):
-        # This part of code creates pairs of source user and retweeted user.
-        # /////////////////////////////////////////////////
-        # all_users_that_retweeted = tools.load_pickle(self.output_path + r"bin\all_users_that_retweeted")
-        # list_of_pairs = list()
-        # for idx, user_id in enumerate(all_users_that_retweeted):
-        #     user_document = self.col_superdocs.find_one({"author_id": user_id})
-        #     for rt_user_id in user_document["rt_authors_list"]:
-        #         list_of_pairs.append((user_id, rt_user_id))
-        # tools.save_pickle(self.output_path + r"bin\list_of_rt_edges_with_duplicates", list_of_pairs)
 
         # list_of_pairs = tools.load_pickle(self.output_path + r"bin\list_of_rt_edges_with_duplicates")
         list_of_top_pairs = tools.load_pickle(self.output_path + r"bin\list_of_top_pairs_10_percent_no_self_rt")
@@ -131,65 +179,14 @@ class RTGraph:
                             name_pair1 = str(rt_pair[1])
                         csv_file.write(name_pair0 + ", " + name_pair1 + ", " + str(num_of_rts) + "\n")
 
-    # Keeping retweets only between the top X percent of users based on the number of retweets they have received
-    # in-degree in the retweet di_graph
-    def connections_of_authorities(self):
-        set_of_all_nodes = set()
-        list_of_pairs = tools.load_pickle(self.output_path + r"bin\list_of_rt_edges_with_duplicates")
-        for rt_pair in list_of_pairs:
-            set_of_all_nodes.add(rt_pair[0])
-            set_of_all_nodes.add(rt_pair[1])
-        counted_list_of_pairs = Counter(list_of_pairs)
-        for rt_pair, num_of_rts in counted_list_of_pairs.items():
-            self.directed_graph.add_edge(rt_pair[0], rt_pair[1], weight=num_of_rts)
-        in_degrees = list(self.directed_graph.in_degree)
-        # This is 5% of all the nodes on the retweet network.
-        top_in_degrees = sorted(in_degrees, key=itemgetter(1), reverse=True)[:180]
-        # indexing for lookups
-        top_users = {s[0] for s in top_in_degrees}
-        list_of_top_pairs = dict()
-        for idx, top_user in enumerate(top_in_degrees):
-            print(idx)
-            for rt_pair, num_of_rts in counted_list_of_pairs.items():
-                if top_user[0] == rt_pair[0] and rt_pair[1] in top_users:
-                    list_of_top_pairs[rt_pair] = num_of_rts
-        # removing self retweets.
-        pairs_to_be_removed = list()
-        for top_pair in list_of_top_pairs:
-            if top_pair[0] == top_pair[1]:
-                pairs_to_be_removed.append(top_pair)
-        print()
-        for t_pair in pairs_to_be_removed:
-            del list_of_top_pairs[t_pair]
-        # tools.save_pickle(self.output_path + r"bin\list_of_top_pairs_10_percent_no_self_rt", list_of_top_pairs)
-
-    def creation_of_digraph(self):
-        list_of_top_pairs = tools.load_pickle(self.output_path + r"bin\list_of_top_pairs_1_percent_no_self_rt")
-        for rt_pair, num_of_rts in list_of_top_pairs.items():
-            self.directed_graph.add_edge(rt_pair[0], rt_pair[1], weight=num_of_rts)
-
-
-    def mongo_tests(self):
-        dumy_list = [1, 2, 3, 4, 5, 6]
-        db = self.client.Climate_Change_Tweets
-        collection = db.test_base
-        collection.insert_one({"author_id": 1, "author_username": "gg", "id_list": dumy_list})
-
-    def backboning_graph(self):
-        table, nnodes, nnedges = backboning.read("/path/to/input", "column_of_interest")
-        nc_table = backboning.noise_corrected(table)
-        nc_backbone = backboning.thresholding(nc_table, threshold_value)
-        backboning.write(nc_backbone, "network_name", "nc", "/path/to/output")
-        print()
-
 
 if __name__ == "__main__":
     climate_rt_graph = RTGraph()
     # climate_rt_graph.seek_retweets()
     # climate_rt_graph.mongo_tests()
     # climate_rt_graph.populate_network()
-    # climate_rt_graph.connections_of_authorities()
-    climate_rt_graph.create_graph_files()
+    climate_rt_graph.connections_of_authorities()
+    # climate_rt_graph.create_graph_files()
     # climate_rt_graph.create_csv_file()
     # climate_rt_graph.creation_of_digraph()
     # a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\Climate_Changed\I_O\\"
