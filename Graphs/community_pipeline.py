2371071da9b05c53a6c37340c0c39bf94a0b768e
imoutidi <imoutidi@gmail.com>
2023-06-05 17:42:12 +0200

    Automated community detection of all percentage graphs and creation of communtiy word clouds done!

--- Graphs/community_pipeline.py
+++ Graphs/community_pipeline.py
@@ -1,29 +1,36 @@
-import re
+import time
 import csv
 # Package name is community but refer to python-louvain on pypi
 import community
 # import infomap
 import networkx as nx
+import matplotlib.pyplot as plt
 
-from operator import itemgetter
-from collections import defaultdict
-from pymongo import MongoClient
+import numpy as np
+from wordcloud import WordCloud
+from PIL import Image
 from Tool_Pack import tools
 
 
 class CommunityPipeline:
 
-    def __init__(self, percentage):
+    def __init__(self, percentage, random_seed):
         self.percent = percentage
-        self.path = r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\Climate_Changed\I_O\\"
-        self.input_path = self.path + r"Graph_files\\" + str(self.percent) + "_percent\\"
-        self.output_path = self.path + r"Communities\\" + str(self.percent) + "_percent\\"
+        self.random_seed = random_seed
+        self.path = r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\Climate_Changed\\"
+        self.input_path = self.path + r"I_O\Graph_files\\" + str(self.percent) + "_percent\\"
+        self.output_path = self.path + r"I_O\Communities\\" + str(self.percent) + "_percent\\"
         self.author_ids_to_labels = dict()
         self.directed_graph = nx.DiGraph()
         # Tuple with author_usernames and their twitter ids that are part of a detected community in the graph.
         # Both sorted based on their in_degree descending.
         self.top_com_nodes = list()
         self.allcoms = list()
+        self.color_dict = {0: "#ff5793", 1: "#91cda2", 2: "#00996d", 3: "#e2bd6a", 4: "#2a4b67", 5: "#00deeb",
+                           6: "#97c8e6", 7: "#b44806", 8: "#8c58a6", 9: "#ff5053", 10: "#c7a6ac", 11: "#68f4d2",
+                           12: "#00c4ff", 13: "#962f4b", 14: "#f9a200", 15: "#afc400", 16: "#ff7b00", 17: "#63b939",
+                           18: "#602f4b", 19: "#008198", 20: "#402f2b", 21: "#afc400", 22: "#39e200", 23: "#ff7aff",
+                           24: "#ff7a11"}
 
     def creation_of_digraph(self):
         # Creating author_id to author username/label in Twitter
@@ -42,7 +49,8 @@ class CommunityPipeline:
 
     def community_detection(self, com_size_threshold=0.01):
         # communities = community.best_partition(self.directed_graph, weight="weight", resolution=1.)
-        communities = nx.community.louvain_communities(self.directed_graph, weight="weight", resolution=1, seed=123)
+        communities = nx.community.louvain_communities(self.directed_graph, weight="weight", resolution=1,
+                                                       seed=self.random_seed)
         self.allcoms = communities
         # Calculating the actual number of X% of the graphs nodes.
         com_threshold_size = len(self.directed_graph) * com_size_threshold
@@ -54,13 +62,42 @@ class CommunityPipeline:
                 com_labels.append(self.author_ids_to_labels[node_id])
             if len(com_nodes) > com_threshold_size:
                 self.top_com_nodes.append((com_labels, com_nodes))
-            print()
         # We sort the list of the top communities based on their size
         self.top_com_nodes = sorted(self.top_com_nodes, key=lambda x: len(x[1]), reverse=True)
 
+    def create_worldclouds(self):
+        mask = np.array(Image.open(self.path + r"Images\c1.png"))
+        input_dict = [{"a": 20, "b": 30, "c": 15, "d": 20, "e": 50, "f": 15, "g": 30,
+                       "h": 20, "i": 10, "j": 10, "k": 17, "l": 30, "m": 40, "n": 20,
+                       "o": 30, "p": 10, "q": 50, "r": 40, "s": 20, "t": 10, "w": 30}]
+        list_of_input_dicts = self.convert_to_list_of_dict(self.top_com_nodes)
+
+        # for idx, com in enumerate(anno_coms):
+        for idx, community_dict in enumerate(list_of_input_dicts):
+            wc = WordCloud(background_color=self.color_dict[idx], mask=mask, max_words=60, prefer_horizontal=1,
+                           contour_width=70, collocations=False, margin=1, width=660, height=660, contour_color="black",
+                           color_func=lambda *args, **kwargs: (0, 0, 0))
+            wc.generate_from_frequencies(community_dict)
+            wc.to_file(self.output_path + "community_" + str(idx) + ".png")
+
+    def convert_to_list_of_dict(self, top_communities, number_of_users_to_display=50):
+        all_com_usernames_dict_list = list()
+        for t_community in top_communities:
+            user_in_degree_dict = dict()
+            for username, author_id in zip(t_community[0][:number_of_users_to_display],
+                                           t_community[1][:number_of_users_to_display]):
+                user_in_degree_dict[username] = self.directed_graph.in_degree[author_id]
+            all_com_usernames_dict_list.append(user_in_degree_dict)
+        return all_com_usernames_dict_list
+
 
 if __name__ == "__main__":
-    rts_pipeline = CommunityPipeline(percentage=1)
+    rts_pipeline = CommunityPipeline(percentage=100, random_seed=123)
     rts_pipeline.creation_of_digraph()
+    start_time = time.perf_counter()
     rts_pipeline.community_detection()
-    print()
+    print("Community detection processing time: " + str(time.perf_counter() - start_time) + " seconds")
+    # rts_pipeline.plot_graphs()
+    start_time = time.perf_counter()
+    rts_pipeline.create_worldclouds()
+    print("Wordclouds creation processing time: " + str(time.perf_counter() - start_time) + " seconds")
