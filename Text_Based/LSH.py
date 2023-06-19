f41bc8b8425047dd54f81a9ef09782f4413b498c
imoutidi <imoutidi@gmail.com>
2023-06-19 12:09:18 +0200

    Still working with LSH.

--- Text_Based/LSH.py
+++ Text_Based/LSH.py
@@ -1,11 +1,21 @@
 from Tool_Pack import tools
 
 
+a = "flying fish flew by the space station"
+b = "we will not allow you to bring your pet armadillo along"
+c = "he figured a few sticks of dynamite were easier than a fishing pole to catch fish"
+k = 2
 
 
-
-
+def shingle(text: str, k: int):
+    shingle_set = []
+    for i in range(len(text) - k+1):
+        shingle_set.append(text[i:i+k])
+    return set(shingle_set)
 
 
 if __name__ == "__main__":
-    print()
+    a = shingle(a, k)
+    b = shingle(b, k)
+    c = shingle(c, k)
+    print(a)
