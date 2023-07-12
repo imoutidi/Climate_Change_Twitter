b36b6273d8390bf472abcc5613b9ad2727fa0c21
imoutidi <imoutidi@gmail.com>
2023-07-12 14:15:22 +0200

    Added backboning code as well.

--- Text_Based/LSH.py
+++ Text_Based/LSH.py
@@ -1,10 +1,10 @@
 from Tool_Pack import tools
 from LocalitySensitiveHashing import *
 from pymongo import MongoClient
+# in case you need this: pip install BitVector
+
 
 # TODO retrieve bert vectors from the mongo database
-
-
 def get_bert_vectors():
     client = MongoClient('localhost', 27017)
     db = client.Climate_Change_Tweets
@@ -12,17 +12,44 @@ def get_bert_vectors():
     collection_superdocs = db.super_documents
     # author_record = collection_superdocs.find_one({"author_id": author_id})
     cursor = collection_superdocs.find({})
-    for document in cursor:
-        print(document["bert_vector"])
-        keep_printing = input("Keep printing")
-        if keep_printing != "Y":
-            break
+    with open(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\LSH_files\\"
+              r"doc_vectors.csv", "w") as lsh_vectors:
+        doc_counter = 0
+        for document in cursor:
+            lsh_vectors.write("sample_" + str(doc_counter) + "," + document["bert_vector"] + "\n")
+            print(doc_counter)
+            doc_counter += 1
 
 
 #  LSH
+def LSHing():
+    lsh = LocalitySensitiveHashing(
+        datafile=r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\LSH_files\doc_vectors.csv",
+        dim=768,  # bert vector dimension
+        r=50,  # number of rows in each band for r-wise AND in each band
+        b=100,  # number of bands for b-wise OR over all b bands
+        expected_num_of_clusters=10,
+        debug=0,
+    )
+    lsh.get_data_from_csv()
+    lsh.show_data_for_lsh()
+    lsh.initialize_hash_store()
+    lsh.hash_all_data()
+    lsh.display_contents_of_all_hash_bins_pre_lsh()
+
+    similarity_groups = lsh.lsh_basic_for_neighborhood_clusters()
+    coalesced_similarity_groups = lsh.merge_similarity_groups_with_coalescence(similarity_groups)
+
+    merged_similarity_groups = lsh.merge_similarity_groups_with_l2norm_set_based(coalesced_similarity_groups)
+
+    lsh.evaluate_quality_of_similarity_groups(merged_similarity_groups)
+
+    print("\n\nWriting the clusters to file 'clusters.txt'")
+    lsh.write_clusters_to_file(merged_similarity_groups, "clusters.txt")
 
 
 if __name__ == "__main__":
-    get_bert_vectors()
+    # get_bert_vectors()
+    LSHing()
 
 
