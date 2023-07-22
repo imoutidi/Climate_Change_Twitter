9907f95b41f9f8d8d0970531fd3b8ac1a6d6987f
imoutidi <imoutidi@gmail.com>
2023-07-22 14:09:33 +0200

    Working on text vector dataset.

--- Near_Neighbors/single_dimension_nn.py
+++ Near_Neighbors/single_dimension_nn.py
@@ -1,3 +1,5 @@
+import langdetect.lang_detect_exception
+
 from Tool_Pack import tools
 from pymongo import MongoClient, ASCENDING, errors
 import tweepy
@@ -21,6 +23,10 @@ from sklearn.metrics.pairwise import cosine_similarity
 import torch
 from transformers import BertTokenizer, BertModel
 
+from langdetect import DetectorFactory    # consistent results
+DetectorFactory.seed = 0
+from langdetect import detect
+
 
 def create_data_set():
     out_path = r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
@@ -32,19 +38,44 @@ def create_data_set():
     for count, document in enumerate(cursor):
         # remove urls
         text = re.sub(r'http\S+', '', document["full_text"])
-        if 40 > len(text) > 20:
+        if len(text) > 100:
             set_of_filler_sentences.add(text)
-        if len(set_of_filler_sentences) > 1000:
-            break
-    tools.save_pickle(out_path + "filler_sentences_20_40", set_of_filler_sentences)
+        # if len(set_of_filler_sentences) > 1000:
+        #     break
+    tools.save_pickle(out_path + "filler_sentences_100", list(set_of_filler_sentences))
+
+def combine_big_and_small_texts():
+    small_texts = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
+                                    r"english_filler_sentences")
+    big_texts = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
+                                  r"filler_sentences_100")
+    dataset_list = list()
+    text_counter = 0
+    for small_t in small_texts:
+        for i in range(10):
+            print()
 
 
 if __name__ == "__main__":
     create_data_set()
-    # a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\filler_sentences")
+    # combine_big_and_small_texts()
+    # a = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
+    #                       r"filler_sentences_60")
+    # b = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
+    #                       r"english_filler_sentences")
+    # b = list()
     # for i in a:
     #     if "warming" not in i:
     #         if "change" not in i:
-    #             print(i)
+    #             try:
+    #                 if detect(i) == "en":
+    #                     b.append(i)
+    #             except langdetect.lang_detect_exception.LangDetectException as exLang:
+    #                 print("----------------------------------------------------")
+    #                 print(i)
+    #                 print(exLang)
+    #                 print("----------------------------------------------------")
+    # tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Near_Neighbors\I_O\\"
+    #                   r"english_filler_sentences", b)
     print()
 
