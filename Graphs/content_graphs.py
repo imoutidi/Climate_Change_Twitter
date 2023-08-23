715902a443624ed4ae055ddbb3335e4c285079d8
imoutidi <imoutidi@gmail.com>
2023-08-23 14:50:08 +0200

    need to run them for every year in parallel.

--- Graphs/content_graphs.py
+++ Graphs/content_graphs.py
@@ -11,31 +11,35 @@ import pickle
 def index_query():
     all_tweets_ids = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\\"
                                        r"Climate_Changed\I_O\bin\all_tweets_ids")
+    tweets_subset = all_tweets_ids[800000:900000]
+
+    del all_tweets_ids
+
     client = MongoClient('localhost', 27017)
     db = client.Climate_Change_Tweets
     collection_tweets = db.tweet_documents
 
     labels_distances_dict = defaultdict(list)
+    except_counter = 0
 
     for nn_index_id in range(20):
-        print(nn_index_id)
+        print("LSH index number: " + str(nn_index_id))
         current_nn_index = tools.load_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\\"
                                              r"I_O\Datasets\LSH_files\indexes\tweets_index_" + str(nn_index_id))
-        for idx, tweet_id in enumerate(all_tweets_ids[:2000000]):
+        for idx, tweet_id in enumerate(tweets_subset):
             print(idx)
             doc_record = collection_tweets.find_one({"tweet_id": tweet_id})
             bert_vector_list = list()
             bert_vector_list.append(np.array(doc_record["bert_vector"]))
             bert_array = np.vstack(bert_vector_list)
-            except_counter = 0
             try:
-                labels, distances = current_nn_index.knn_query(bert_array, k=2000)
+                labels, distances = current_nn_index.knn_query(bert_array, k=800)
                 labels_distances_dict[tweet_id].append((labels, distances))
             except Exception as e:
                 except_counter += 1
                 print("Exception number: " + str(except_counter))
     tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\I_O\Datasets\LSH_files\\"
-                      r"Computations\labels_distances_dict_1")
+                      r"Computations\labels_distances_dict_8", labels_distances_dict)
 
     # Keep that to remember how to acces the actuall tweets ids and text.
     # print("Top closests matches:")
