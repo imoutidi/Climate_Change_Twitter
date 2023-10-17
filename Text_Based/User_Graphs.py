0dbe3be137a3145a06e27900759312adb49ff8e8
imoutidi <imoutidi@gmail.com>
2023-10-17 20:07:16 +0200

    Forming Graphs. Wordcloud construction.

--- Text_Based/User_Graphs.py
+++ Text_Based/User_Graphs.py
@@ -18,18 +18,28 @@ class GraphCreator:
         for user_id, keyword_list in user_keyword_dict.items():
             for keyword_tuple in keyword_list:
                 self.word_to_users_inverted_index[keyword_tuple[0]].append((user_id, keyword_tuple[1]))
-                print()
+        tools.save_pickle(r"C:\Users\irmo\PycharmProjects\Climate_Change_Twitter\Text_Based\\"
+                          r"I_O\Indexes\word_to_user_more_than_one_tweet", self.word_to_users_inverted_index)
 
 
     def create_relations(self):
         user_keyword_vectors = \
             tools.load_pickle(self.main_path + r"I_O\Indexes\normalized_user_to_keywords_list_more_than_one_tweet")
+        self.word_to_users_inverted_index = \
+            tools.load_pickle(r"C:\Users\irmo\PycharmProjects\\"
+                              r"Climate_Change_Twitter\Text_Based\I_O\Indexes\word_to_user_more_than_one_tweet")
 
-        print()
+        for user_id, list_of_word_freqs in user_keyword_vectors.items():
+            for word_tuple in list_of_word_freqs:
+                for idx, user_freq_tuple in enumerate(self.word_to_users_inverted_index[word_tuple[0]]):
+                    # the users ids are always sorted in the tuple that are key in
+                    # self.dict_of_user_similarity
 
+                    # self.dict_of_user_similarity[()]
+                    print()
 
 
 if __name__ == "__main__":
     g_creator = GraphCreator()
-    g_creator.create_inverted_index()
+    # g_creator.create_inverted_index()
     g_creator.create_relations()
